2683cb62ad50cbcd40087e1efba338a8bc31d771
stephae <stephanie.smits@hotmail.com>
2024-12-19 15:13:27 +0100

    Update minute.java

--- minute.java
+++ minute.java
@@ -2,22 +2,17 @@ import java.util.Random;
 
 public class RandomMinutes {
     public static void main(String[] args) {
-        // Define the range for random even minutes (e.g., 0 to 58)
         int minMinutes = 0;
         int maxMinutes = 59;
 
-        // Generate a random even number of minutes
         int randomEvenMinutes = getRandomEvenMinutes(minMinutes, maxMinutes);
 
-        // Print the result
         System.out.println("Random Even Minutes: " + randomEvenMinutes);
     }
 
-    // Method to generate a random even number within a range
     public static int getRandomEvenMinutes(int min, int max) {
         Random random = new Random();
 
-        // Ensure the range is valid for even numbers
         int adjustedMin = (min % 2 == 0) ? min : min + 1;
         int adjustedMax = (max % 2 == 0) ? max : max - 1;
 
@@ -25,7 +20,6 @@ public class RandomMinutes {
             throw new IllegalArgumentException("No even numbers in the given range");
         }
 
-        // Generate a random even number
         int randomEven = random.nextInt((adjustedMax - adjustedMin) / 2 + 1) * 2 + adjustedMin;
         return randomEven;
     }
